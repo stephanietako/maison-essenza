8fcedd68e40f9ff785925eebe6c792363b545f89
stephanietako <gabeshine@live.fr>
2024-07-29 15:14:38 +0200

    wip: starting peoject part3/rectification cache

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -1,78 +1,41 @@
-// // pages/api/auth/callback.js
-
-// export default async function handler(req, res) {
-//   if (req.method === "GET") {
-//     const { code } = req.query;
-
-//     try {
-//       const response = await fetch(
-//         "https://api.instagram.com/oauth/access_token",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/x-www-form-urlencoded",
-//           },
-//           body: new URLSearchParams({
-//             client_id: process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID,
-//             client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
-//             grant_type: "authorization_code",
-//             redirect_uri:
-//               "https://maison-essenza-website.vercel.app/auth/callback",
-//             code: code,
-//           }),
-//         }
-//       );
-
-//       if (!response.ok) {
-//         console.error("Failed to fetch access token");
-//         return res.status(400).json({ error: "Failed to fetch access token" });
-//       }
-
-//       const data = await response.json();
-//       // Sauvegarder le token d'accès et d'autres informations utiles
-//       // Vous pouvez l'enregistrer dans une base de données ou le stocker dans un cookie sécurisé
-
-//       res.status(200).json({ access_token: data.access_token });
-//     } catch (error) {
-//       console.error("Server error", error);
-//       res.status(500).json({ error: "Server error" });
-//     }
-//   } else {
-//     res.setHeader("Allow", ["GET"]);
-//     res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
-// }
-// pages/api/instagram.js
+// pages/api/auth/callback.js
 
 export default async function handler(req, res) {
-  const accessToken = process.env.INSTAGRAM_KEY; // Assurez-vous que ce jeton est valide
-
   if (req.method === "GET") {
+    const { code } = req.query;
+
     try {
       const response = await fetch(
-        `https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink&access_token=${accessToken}`,
+        "https://api.instagram.com/oauth/access_token",
         {
-          // Désactive le cache pour la requête
+          method: "POST",
           headers: {
-            "Cache-Control": "no-store",
+            "Content-Type": "application/x-www-form-urlencoded",
           },
+          body: new URLSearchParams({
+            client_id: process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID,
+            client_secret: process.env.INSTAGRAM_CLIENT_SECRET,
+            grant_type: "authorization_code",
+            redirect_uri:
+              "https://maison-essenza-website.vercel.app/auth/callback",
+            code: code,
+          }),
         }
       );
 
       if (!response.ok) {
-        throw new Error("Failed to fetch data from Instagram");
+        console.error("Failed to fetch access token");
+        return res.status(400).json({ error: "Failed to fetch access token" });
       }
 
       const data = await response.json();
-      const recentPost = data.data[0]; // Récupérer le premier post (le plus récent)
+      // Sauvegarder le token d'accès et d'autres informations utiles
+      // Vous pouvez l'enregistrer dans une base de données ou le stocker dans un cookie sécurisé
 
-      // Configurez l'en-tête pour éviter le cache
-      res.setHeader("Cache-Control", "no-store");
-
-      res.status(200).json(recentPost);
+      res.status(200).json({ access_token: data.access_token });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Failed to fetch data" });
+      console.error("Server error", error);
+      res.status(500).json({ error: "Server error" });
     }
   } else {
     res.setHeader("Allow", ["GET"]);
