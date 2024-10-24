1e8babe427faad090a1b2a1147ff82df964e8603
stephanietako <gabeshine@live.fr>
2024-10-25 00:07:31 +0200

    wip: refacto prev3

--- app/components/Hero/Hero.jsx
+++ app/components/Hero/Hero.jsx
@@ -1,25 +1,79 @@
+// "use client";
+// import React from "react";
+// // Styles
+// import styles from "./styles.module.scss";
+// const Hero = ({ style }) => {
+//   return (
+//     <div className="hero" id="hero" style={style}>
+//       <video
+//         id={styles.video}
+//         autoPlay
+//         muted
+//         playsInline
+//         loop
+//         className="video"
+//       >
+//         <source src={"/video/herovideo.mp4"} />
+//         Your browser does not support the video tag.
+//       </video>
+//     </div>
+//   );
+// };
+
+// export default Hero;
+//////////////:
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+// Importation du composant Image de Next.js
+import Image from "next/image";
+// Styles
+import styles from "./styles.module.scss";
 
 const Hero = ({ style }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  // Fonction pour vérifier la taille de l'écran
+  const handleResize = () => {
+    setIsMobile(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    // Vérifiez la taille de l'écran lors du premier rendu
+    handleResize();
+
+    // Écoutez les changements de taille de l'écran
+    window.addEventListener("resize", handleResize);
+
+    // Nettoyez l'écouteur d'événements
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="hero" id="hero" style={style}>
-      <video
-        style={{
-          display: "flex",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-        }}
-        src="/video/herovideo.mp4"
-        type="video/mp4"
-        autoPlay
-        muted
-        loop
-      />
+      <div>
+        {isMobile ? (
+          <Image
+            src="/assets/hero-gif.gif"
+            alt="GIF alternative"
+            sizes="(max-width: 800px) 100vw, 1200px 70vw  1200px 50vw"
+            width={800}
+            height={700}
+            unoptimized
+            className={styles.gif}
+          />
+        ) : (
+          <video
+            id={styles.video}
+            playsInline
+            loop
+            muted
+            autoPlay
+            src="/video/herovideo.mp4"
+          />
+        )}
+      </div>
     </div>
   );
 };
